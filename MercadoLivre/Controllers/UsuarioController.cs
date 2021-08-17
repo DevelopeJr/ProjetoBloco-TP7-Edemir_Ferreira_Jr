532e30e1102860b849d9b0b76d593836c59c02fe
DevelopeJr <edemir.ferreira.jr@gmail.com>
2021-08-17 09:31:27 -0300

    UserId

--- MercadoLivre/Controllers/UsuarioController.cs
+++ MercadoLivre/Controllers/UsuarioController.cs
@@ -104,7 +104,7 @@ namespace MercadoLivre.Controllers
         // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,DataNascimento,Email,Online,Offline")] Usuario usuario)
+        public async Task<IActionResult> Edit(int id, [Bind("Id,Nome,DataNascimento,Email,Online,Offline, UserId")] Usuario usuario)
         {
             if (id != usuario.Id)
             {
